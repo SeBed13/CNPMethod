95af07a3f45066a06d0bbe4a7a0ff8234466a073
SeBed13 <bedregianusebastian@gmail.com>
2023-01-07 11:25:55 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -40,114 +41,115 @@ public class Main {
                 break;
             case '6':
                 prefix = "20";
-    break;
+                break;
             default:
                 prefix = "INVALID";
-    break;
+                break;
         }
         return prefix;
     }
     public static String countyOfBirth(int twoNumbers){
         String county = "";
-    if(twoNumbers == 1){
-        System.out.println("Alba");
+    if(twoNumbers == 01){
+        System.out.println("County: Alba");
     } else if (twoNumbers == 02) {
-        System.out.println("Arad");
+        System.out.println("County: Arad");
     } else if (twoNumbers == 03) {
-        System.out.println("Arges");
+        System.out.println("County: Arges");
     } else if (twoNumbers == 04) {
-        System.out.println("Bacau");
+        System.out.println("County: Bacau");
     } else if (twoNumbers == 05) {
-        System.out.println("Bihor");
+        System.out.println("County: Bihor");
     } else if (twoNumbers == 06) {
-        System.out.println("Bistrita-Nasaud");
+        System.out.println("County: Bistrita-Nasaud");
     } else if (twoNumbers == 07) {
-        System.out.println("Botosani");
+        System.out.println("County: Botosani");
     } else if (twoNumbers == 8) {
-        System.out.println("Brasov");
+        System.out.println("County: Brasov");
     } else if (twoNumbers == 9) {
-        System.out.println("Braila");
+        System.out.println("County: Braila");
     }else if (twoNumbers == 10) {
-        System.out.println("Buzau");
+        System.out.println("County: Buzau");
     } else if (twoNumbers == 11) {
-        System.out.println("Caras-Severin");
+        System.out.println("County: Caras-Severin");
     } else if (twoNumbers == 12) {
-        System.out.println("Cluj");
+        System.out.println("County: Cluj");
     } else if (twoNumbers == 13) {
-        System.out.println("Constanta");
+        System.out.println("County: Constanta");
     } else if (twoNumbers == 14) {
-        System.out.println("Covasna");
+        System.out.println("County: Covasna");
     } else if (twoNumbers == 15) {
-        System.out.println("Dambovita");
+        System.out.println("County: Dambovita");
     } else if (twoNumbers == 16) {
-        System.out.println("Dolj");
+        System.out.println("County: Dolj");
     } else if (twoNumbers == 17) {
-        System.out.println("Galati");
+        System.out.println("County: Galati");
     } else if (twoNumbers == 18) {
-        System.out.println("Gorj");
+        System.out.println("County: Gorj");
     } else if (twoNumbers == 19){
-        System.out.println("Harghita");
+        System.out.println("County: Harghita");
     } else if (twoNumbers == 20) {
-        System.out.println("Hunedoara");
+        System.out.println("County: Hunedoara");
     } else if (twoNumbers == 21) {
-        System.out.println("Ialomita");
+        System.out.println("County: Ialomita");
     } else if (twoNumbers == 22) {
-        System.out.println("Iasi");
+        System.out.println("County: Iasi");
     } else if (twoNumbers == 23) {
-        System.out.println("Ilfov");
+        System.out.println("County: Ilfov");
     } else if (twoNumbers == 24) {
-        System.out.println("Maramures");
+        System.out.println("County: Maramures");
     } else if (twoNumbers == 25) {
-        System.out.println("Mehedinti");
+        System.out.println("County: Mehedinti");
     } else if (twoNumbers == 26) {
-        System.out.println("Mures");
+        System.out.println("County: Mures");
     } else if (twoNumbers == 27) {
-        System.out.println("Neamt");
+        System.out.println("County: Neamt");
     } else if (twoNumbers == 28) {
-        System.out.println("Olt");
+        System.out.println("County: Olt");
     } else if (twoNumbers == 29) {
-        System.out.println("Prahova");
+        System.out.println("County: Prahova");
     } else if (twoNumbers == 30) {
-        System.out.println("Satu Mare");
+        System.out.println("County: Satu Mare");
     } else if (twoNumbers == 31) {
-        System.out.println("Salaj");
+        System.out.println("County: Salaj");
     } else if (twoNumbers == 32) {
-        System.out.println("Sibiu");
+        System.out.println("County: Sibiu");
     } else if (twoNumbers == 33) {
-        System.out.println("Suceava");
+        System.out.println("County: Suceava");
     } else if (twoNumbers == 34) {
-        System.out.println("Teleorman");
+        System.out.println("County: Teleorman");
     } else if (twoNumbers == 35) {
-        System.out.println("Timis");
+        System.out.println("County: Timis");
     } else if (twoNumbers == 36) {
-        System.out.println("Tulcea");
+        System.out.println("County: Tulcea");
     } else if (twoNumbers == 37) {
-        System.out.println("Vaslui");
+        System.out.println("County: Vaslui");
     } else if (twoNumbers == 38) {
-        System.out.println("Valcea");
+        System.out.println("County: Valcea");
     } else if (twoNumbers == 39) {
-        System.out.println("Vrancea");
+        System.out.println("County: Vrancea");
     } else if (twoNumbers == 40) {
-        System.out.println("Bucuresti");
+        System.out.println("County: Bucuresti");
     } else if (twoNumbers == 41) {
-        System.out.println("Bucuresti - Sector 1");
+        System.out.println("County: Bucuresti - Sector 1");
     } else if (twoNumbers == 42) {
-        System.out.println("Bucuresti - Sector 2");
+        System.out.println("County: Bucuresti - Sector 2");
     } else if (twoNumbers == 43) {
-        System.out.println("Bucuresti - Sector 3");
+        System.out.println("County: Bucuresti - Sector 3");
     } else if (twoNumbers == 44) {
-        System.out.println("Bucuresti - Sector 4");
+        System.out.println("County: Bucuresti - Sector 4");
     } else if (twoNumbers == 45) {
-        System.out.println("Bucuresti - Sector 5");
+        System.out.println("County: Bucuresti - Sector 5");
     } else if (twoNumbers == 46) {
-        System.out.println("Bucuresti - Sector 6");
+        System.out.println("County: Bucuresti - Sector 6");
     } else if (twoNumbers == 51) {
-        System.out.println("Calarasi");
+        System.out.println("County: Calarasi");
     } else if (twoNumbers == 52) {
-        System.out.println("Giurgiu");
+        System.out.println("County: Giurgiu");
     }
         return county;
     }
+
     public static int age(String socialNumber){
         String[] birthDateFormatted = formatDateAsString(getBirthDateFromCNPAsString(socialNumber));
 
@@ -159,7 +161,7 @@ public class Main {
         Date date = new Date();
 
         String currentDate = format.format(date);
-        System.out.println(currentDate);
+        System.out.println("Current day is:" + currentDate);
         String[] currentDateFormatted = formatDateAsString(currentDate);
 
         int currentDay = Integer.parseInt(currentDateFormatted[0]);
@@ -176,20 +178,25 @@ public class Main {
         }
         return age;
     }
-
-    public static void main(String[] args) {
-       String socialNumber = "1700704372247";
-        System.out.println(isCNPValid(socialNumber.length()));
-        char sex = socialNumber.charAt(0);
-        if (sex == '1' || sex == '3' || sex == '5' || sex == '7') {
-            System.out.println("Male");
-        } else if (sex == '2' || sex == '4' || sex == '6' || sex == '8') {
-            System.out.println("Female");
+    public static String genderByCNP(String socialNumber){
+        char firstCharacter = socialNumber.charAt(0);
+        String sex;
+        if (firstCharacter == '1' || firstCharacter == '3' || firstCharacter == '5' || firstCharacter == '7') {
+            sex = "Male";
+        } else if (firstCharacter == '2' || firstCharacter == '4' || firstCharacter == '6' || firstCharacter == '8') {
+            sex = "Female";
         } else {
-            System.out.println("Alien");
+            sex = "Alien";
         }
+        return sex;
+    }
+    public static void main(String[] args) {
+       String socialNumber = "1960320086324";
+        isCNPValid(socialNumber.length());
+        System.out.println("CNP:" + socialNumber);
+        System.out.println("Sex: " + genderByCNP(socialNumber));
         String county = countyOfBirth(Integer.parseInt(socialNumber.substring(7, 9)));
-        System.out.println(getBirthDateFromCNPAsString(socialNumber));
+        System.out.println("Date of birth: " + getBirthDateFromCNPAsString(socialNumber));
 
         System.out.println("Current age is: " + age(socialNumber) + " years old.");
 
